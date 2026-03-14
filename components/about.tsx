223deeabd647d9ad4e76f80285db96a6db59c050
Daman <damanpreetk117@gmail.com>
2026-03-14 22:52:30 +0530

    few more improvements

--- components/about.tsx
+++ components/about.tsx
@@ -12,7 +12,7 @@ export const About = () => {
             <div className="flex flex-col md:flex-row gap-1 ">
                 <motion.div initial={{ opacity: 0, filter: "blur(2px)" }}
                     whileInView={{ opacity: 1, filter: "blur(0px)" }}
-                    transition={{ duration: 0.1, ease: "easeInOut" }}
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
                     className="w-24 md:w-32 h-24 md:h-28 relative overflow-hidden rounded-full shadow-2xl shadow-accent/30">
                     <Image src={"/moana3.jpg"} fill alt={"moana"}
                         className="object-cover rounded-full" />
@@ -20,7 +20,7 @@ export const About = () => {
 
                 <motion.div initial={{ opacity: 0, filter: "blur(2px)" }}
                     whileInView={{ opacity: 1, filter: "blur(0px)" }}
-                    transition={{ duration: 0.2, ease: "easeInOut" }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
                     className="flex flex-col gap-1 md:gap-2 p-2 md:p-3">
                     <h1 className="text-xl md:text-2xl font-semibold">
                         daman<span className="md:hidden">preet kaur</span>
@@ -36,7 +36,7 @@ export const About = () => {
             <div className="flex flex-col gap-6 my-6">
                 <motion.div initial={{ opacity: 0, filter: "blur(2px)" }}
                     whileInView={{ opacity: 1, filter: "blur(0px)" }}
-                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                    transition={{ duration: 0.6, ease: "easeInOut" }}
                     className="flex items-start gap-2">
                     <div className="rounded-full bg-secondary size-2 animate-pulse shrink-0 mt-2" />
                     <p className="md:text-base md:pr-14 lg:pr-20 text-neutral-600">much like Moana listening to the ocean,i am drawn to exploring the web -
@@ -47,7 +47,7 @@ export const About = () => {
                 <div className="flex flex-col gap-2 md:gap-3">
                     <motion.div initial={{ opacity: 0, filter: "blur(2px)" }}
                         whileInView={{ opacity: 1, filter: "blur(0px)" }}
-                        transition={{ duration: 0.4, ease: "easeInOut" }}
+                        transition={{ duration: 0.7, ease: "easeInOut" }}
                         className="flex items-start gap-2">
                         <div className="rounded-full bg-accent size-2 animate-pulse shrink-0 mt-2" />
                         <p className="text-bases md:pr-14 lg:pr-20 text-neutral-600 ">
@@ -59,7 +59,7 @@ export const About = () => {
                         {techStack.map((item, i) => (
                             <motion.div key={item} initial={{ opacity: 0, filter: "blur(2px)" }}
                                 whileInView={{ opacity: 1, filter: "blur(0px)" }}
-                                transition={{ duration: 0.5, ease: "easeIn", delayChildren: i * 0.04 }}
+                                transition={{ duration: 0.8, ease: "easeIn", delayChildren: i * 0.04 }}
                                 className="text-sm text-neutral-700 bg-accent/8 hover:bg-accent/14 transition-colors duration-300 border 
                             border-accent/30 font-semibold px-2 md:px-3 lg:px-4 py-1 cursor-pointer rounded-lg">
                                 {item}
@@ -70,7 +70,7 @@ export const About = () => {
 
                 <motion.div initial={{ opacity: 0, filter: "blur(2px)" }}
                     whileInView={{ opacity: 1, filter: "blur(0px)" }}
-                    transition={{ duration: 0.7, ease: "easeInOut" }}
+                    transition={{ duration: 1, ease: "easeInOut" }}
                     className="mt-14 bg-neutral-100 rounded-xl px-3 py-2 pl-3 flex flex-col gap-3">
                     <p className="md:text-base text-neutral-700">
                         Good design is actually a lot harder to notice than poor design,
--- components/boat-animation.tsx
+++ components/boat-animation.tsx
@@ -9,7 +9,7 @@ export const BoatAnimation = () => {
         <AnimatePresence>
             <motion.div initial={{ opacity: 0, filter: "blur(2px)" }}
                 whileInView={{ opacity: 1, filter: "blur(0px)" }}
-                transition={{ ease: "easeInOut", duration: 1, delay: 0.2 }}
+                transition={{ ease: "easeInOut", duration: 1.2, delay: 0.2 }}
                 exit={{ opacity: 0, filter: "blur(2px)" }}
                 className="w-full max-w-3xl mx-auto flex gap-3 ">
                 <Lottie animationData={movingBoatAnimation} />
